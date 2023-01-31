c4bb7ba8107e6dc6b6c4269642d0ba70ac0b8801
Your Name <you@example.com>
2023-01-31 11:53:42 -0600

    Set up connection to MongoDB cluster

--- server/index.js
+++ server/index.js
@@ -2,3 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+//body-parser will allow us to scale large images. This way we can give parameters.
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://LeahChoco:LeahChoco123@cluster0.mhyzbeq.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//mongoose.set("useFindAndModify", false);
