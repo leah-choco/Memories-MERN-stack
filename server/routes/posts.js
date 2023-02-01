556e84039a6c58865c369147d485dffebd4ee989
Your Name <you@example.com>
2023-02-01 12:05:59 -0600

    Adding logic to more of the controllers

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 //This file structure will allow us to easily keep this file clean and add logic into controllers
 router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
