4e3246c1704fab72d7fc9ce4bd7e376326ec5407
Your Name <you@example.com>
2023-02-02 13:29:05 -0600

    Setting up files with Redux

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,8 +1,19 @@
 import React from "react";
+
+import { FormControl, InputLabel, Input, FormHelperText } from "@mui/material/";
+
 import "./form.css";
 
 const Form = () => {
-  return <h1>Form</h1>;
+  return (
+    <FormControl>
+      <InputLabel htmlFor="my-input">Email address</InputLabel>
+      <Input id="my-input" aria-describedby="my-helper-text" />
+      <FormHelperText id="my-helper-text">
+        We'll never share your email.
+      </FormHelperText>
+    </FormControl>
+  );
 };
 
 export default Form;
